c2efadf6e321ad85412b3f1cae7dc3bec56f81f9
Eddy <briceeddymarcos@gmail.com>
2024-08-27 23:28:13 -0600

    Inicio

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hola
+    render html: "Hola, Mundo"
+  end
+
+  def bienvenida
+    @nombre = "Usuario"
+    render html: "Bienvenido, #{@nombre}!"
+  end
+end
